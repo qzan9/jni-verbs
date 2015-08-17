b25ee847dd5835908c850b4d9db49a5e4e37cf12
anzhongqi <anzhongqi@gmail.com>
2015-08-17 20:51:55 +0800

    added copyright notice header.

--- examples/src/main/c/ibv_getdev/ibv_getdev.c
+++ examples/src/main/c/ibv_getdev/ibv_getdev.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015, AZQ. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
