98bedee56fe2b571b04a7b5b25879a9ea8b704c1
anzhongqi <anzhongqi@gmail.com>
2016-01-09 13:19:44 +0800

    RunJniRdma: microbenchmarking DirectByteBuffer.put 512KB data.

--- src/main/java/ac/ncic/syssw/jni/RunJniRdma.java
+++ src/main/java/ac/ncic/syssw/jni/RunJniRdma.java
@@ -1,10 +1,14 @@
 /*
- * Copyright (c) 2015  AZQ
+ * Exercise the JniRdma native methods and collect microbenchmarking info.
+ *
+ * Author(s):
+ *   azq  @qzan9  anzhongqi@ncic.ac.cn
  */
 
 package ac.ncic.syssw.jni;
 
 import java.nio.ByteBuffer;
+import java.util.Random;
 
 public class RunJniRdma {
 	private RunJniRdma() { }
@@ -21,17 +25,19 @@ public class RunJniRdma {
 		try {
 			RdmaUserConfig userConfig = new RdmaUserConfig(65536, (args.length == 0) ? null : args[0], 9999);
 			ByteBuffer buffer = JniRdma.rdmaInit(userConfig);
+
 			if (userConfig.getServerName() == null) {    // server
 				String data = "it's been a long day without you my friend, and i'll tell you all about it when i see you again ...";
 				System.out.println("data (String): " + data);
-				buffer.putInt(data.length());    // buffer: first half for write.
+				buffer.putInt(data.length());
 				buffer.put(data.getBytes());
-				System.out.println("server writing to remote client buffer through RDMA ...");
+//				System.out.println("server writing to remote client buffer through RDMA ...");
 				JniRdma.rdmaWrite();
 			} else {    // client
 				System.out.println("client reading local buffer ...");
-				while(true) if (buffer.getInt(userConfig.getBufferSize()) > 0) break;    // buffer: second half for read.
-				buffer.position(userConfig.getBufferSize());
+//				while(true) if (buffer.getInt(userConfig.getBufferSize()) > 0) break;
+				while(true) if (buffer.getInt(0) > 0) break;
+//				buffer.position(userConfig.getBufferSize());
 				byte[] bytes = new byte[buffer.getInt()];
 				buffer.get(bytes);
 				String data = new String(bytes);
@@ -51,11 +57,27 @@ public class RunJniRdma {
 			RdmaUserConfig userConfig = new RdmaUserConfig(512, null, 9999);
 			ByteBuffer buffer = JniRdma.rdmaInit(userConfig);
 
-			String data = "it's been a long day without you my friend, and i'll tell you all about it when i see you again ...";
-			System.out.println("data (String): " + data);
-			buffer.put(data.getBytes());
-			System.out.println("server writing to remote client buffer through RDMA ...");
-			JniRdma.rdmaWriteAsync(0, data.length());
+			System.out.println("=== RDMA write benchmarking ===");
+
+			System.out.println("warming up ...");
+			Random random = new Random();
+			byte[] data = new byte[512];
+			for (int i = 0; i < 100; i++) {
+				random.nextBytes(data);
+			}
+
+			long elapsedTimeSum = 0;
+			for (int t = 0; t < 1000; t++) {
+				random.nextBytes(data);
+
+				long startTime = System.nanoTime();
+				buffer.put(data);
+				elapsedTimeSum += System.nanoTime() - startTime;
+			}
+			System.out.printf("average time of putting %d KB data into DirectByteBuffer is %d us us.\n", 512, elapsedTimeSum/1000000);
+
+//			JniRdma.rdmaWrite();
+			JniRdma.rdmaWriteAsync(0, data.length);
 			JniRdma.rdmaPollCq(1);
 		} catch (RdmaException e) {
 			System.out.println("RDMA error! check your native IB/OFED/TCP configuration!");
