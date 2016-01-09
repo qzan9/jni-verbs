89f9e11dd95683a460b9ebd821b959bd11475153
anzhongqi <anzhongqi@gmail.com>
2016-01-09 11:30:30 +0800

    demo_client: a bit enhancement + simple arguments check, + data verification: check the last 4 bytes instead of first 4 bytes.

--- src/main/c/demo_client/demo_client.c
+++ src/main/c/demo_client/demo_client.c
@@ -1,10 +1,24 @@
 /*
- * Copyright (c) 2016  AZQ
+ * a naive demo for test.
+ *
+ * Author(s):
+ *   azq  @qzan9  anzhongqi@ncic.ac.cn
  */
 
 #ifdef __GNUC__
+
+#ifndef _POSIX_C_SOURCE
 #	define _POSIX_C_SOURCE 200809L
+#endif /* _POSIX_C_SOURCE */
+
+#ifndef _GNU_SOURCE
 #	define _GNU_SOURCE
+#endif /* _GNU_SOURCE */
+
+#ifndef _SVID_SOURCE
+#	define _SVID_SOURCE
+#endif /* _SVID_SOURCE */
+
 #endif /* __GNUC__ */
 
 #include <stdio.h>
@@ -16,43 +30,61 @@
 #include <my_rdma.h>
 #include <chk_err.h>
 
+#define DEFAULT_IB_DEVICE       0
+#define DEFAULT_IB_PORT         1
+#define DEFAULT_BUFFER_SIZE   512
+#define DEFAULT_SOCKET_PORT  9999
+
 static struct rdma_context *rctx;
 static struct user_config  *ucfg;
 
 int main(int argc, char **argv)
 {
+	int op;
 	int n0, n1;
-
-	if (argc != 2) {
-		printf("usage: ...\n");
-		return -1;
-	}
 
 	rctx = malloc(sizeof *rctx);
 	ucfg = malloc(sizeof *ucfg);
 	memset(rctx, 0, sizeof *rctx);
 	memset(ucfg, 0, sizeof *ucfg);
 
-	ucfg->ib_dev  = 0;
-	ucfg->ib_port = 1;
-	ucfg->buffer_size = 512;
-	ucfg->server_name = argv[1];
-	ucfg->sock_port = 9999;
+	ucfg->ib_dev      = DEFAULT_IB_DEVICE;
+	ucfg->ib_port     = DEFAULT_IB_PORT;
+	ucfg->buffer_size = DEFAULT_BUFFER_SIZE;
+	ucfg->server_name = NULL;
+	ucfg->sock_port   = DEFAULT_SOCKET_PORT;
 
-	//printf("server_name: %s\n", ucfg->server_name);
+	while ((op = getopt(argc, argv, "b:s:")) != -1) {
+		switch (op) {
+		case 'b':
+			ucfg->buffer_size = atoi(optarg);
+			break;
+		case 's':
+			ucfg->server_name = optarg;
+			break;
+		default:
+			printf("%s -b [buffer size] -s [server hostname]\n", argv[0]);
+			return -1;
+		}
+	}
 
-	srand48(getpid() * time(NULL));
+	//printf("buffer size: %d\n", ucfg->buffer_size);
+	//printf("server name: %s\n", ucfg->server_name);
+
+	if (!ucfg->server_name) {
+		printf("%s -b [buffer size] -s [server hostname]\n", argv[0]);
+		return -1;
+	}
 
 	CHK_NZPI(init_context(rctx, ucfg), "failed to initialize my RDMA context!");
-	n0 = *((int *)rctx->buf);
+	n0 = *((int *)(rctx->buf + rctx->size - sizeof n0));
 	printf("N0: %d\n", n0);
 	CHK_NZPI(connect_to_peer(rctx, ucfg), "failed to connect to peer server!");
 
 	while(1) {
-		printf("%d", n1 = *((int *)rctx->buf));
+		printf("%d", n1 = *((int *)(rctx->buf + rctx->size - sizeof n1)));
 		if (n0 != n1) {
-			printf("\nN0 != N1\n");
-			printf("N1: %d\n", n1);
+			printf("\nDATA (maybe) WRITTEN!\n");
 			break;
 		}
 	}
